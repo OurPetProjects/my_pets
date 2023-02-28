805f02e558220651407e198cb6d3288e55954cbb
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-27 20:09:44 -0600

    baseline tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      // For use in responsiveness
+      xs: "475px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     colors: {
-      'slate': '#2E4052',
-      'yellow': '#FFC857',
-      'blue': '#7293A0',
-      'red': '#8A3431',
-      'white': '#FDFFFB'
-    }
+      // Theme pallete
+      slate: "#2E4052",
+      yellow: "#FFC857",
+      blue: "#7293A0",
+      red: "#8A3431",
+      white: "#FDFFFB",
+    },
+    fontFamily: {
+      // Set chosen font(s) here
+    },
+    extend: {},
   },
-  plugins: [
-     require('@tailwindcss/forms')
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
