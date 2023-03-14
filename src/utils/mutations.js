b751841f2a6701a24bdb760e48e325cdf46204c2
MJ_Shelton <mjshelton12@comcast.net>
2023-03-13 17:47:29 -0700

    Updated user registration

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -11,3 +11,17 @@ export const LOGIN_USER = gql`
         }
     }
 `;
+
+export const REGISTER_USER = gql`
+    mutation register(firstName: String!, lastName: String!, username: String!, email: String, password: String!) {
+        login(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                firstName
+                lastName
+                email
+            }
+        }
+    }
+`;
