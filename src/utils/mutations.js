104f7e986bb6df086f0ddb70da77d7cd5d8f2335
MJ_Shelton <mjshelton12@comcast.net>
2023-03-20 18:34:51 -0700

    Improve functionality for sign up

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const REGISTER_USER = gql`
-    mutation register(firstName: String!, lastName: String!, username: String!, email: String, password: String!) {
+    mutation register($firstName: String!, $lastName: String!, $username: String!, $email: String, $password: String!) {
         login(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
             token
             user {
